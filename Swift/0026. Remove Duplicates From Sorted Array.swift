e5ab01d292b627bebf4aef043c1f8103c1074c18
Richard Cho <abelsteiger@gmail.com>
2024-10-27 01:06:57 +0900

    Daily Commit

--- Swift/0026. Remove Duplicates From Sorted Array.swift
+++ Swift/0026. Remove Duplicates From Sorted Array.swift
@@ -8,20 +8,13 @@ import Foundation
 
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-            guard nums.count > 0 else { return 0 }
-            var count = nums.count, length = 1, lastNum = nums[0], i = 1
-            
-            while i < count {
-                if lastNum == nums[i] {
-                    nums.remove(at: i)
-                    count -= 1
-                } else {
-                    lastNum = nums[i]
-                    length += 1
-                    i += 1
-                }
+        var k = 1;
+        for i in 1..<nums.count {
+            if nums[i] != nums[i - 1] {
+                nums[k] = nums[i]
+                k += 1
             }
-            return length
         }
+        return k
+    }
 }
-
